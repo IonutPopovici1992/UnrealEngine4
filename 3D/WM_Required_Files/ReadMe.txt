cf31260b059632d23aaa6ee1ac8bb3bc852455db
John Wayne <ionutpopovici92@gmail.com>
2017-09-11 00:01:01 +0300

    3D folder + Udemy folder

---
+++ 3D/WM_Required_Files/ReadMe.txt
@@ -0,0 +1,7 @@
+Notes on Splat Maps:
+
+To bring World Machine splat maps into Unity, copy the ReplaceSplatmap.js file into the Assets/Editor folder in your Unity Project.
+
+Copy the Easy Splat.dev macro file into your My Documents/World Machine Documents/Macros folder.
+
+Any questions or comments? Send to support@qt-ent.com!
---
+++ Udemy/Section_01/Intermediate/Build/Win64/UE4Editor/Inc/Section_01/Section_01.generated.dep.h
@@ -0,0 +1,10 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#ifndef SECTION_01_Section_01GameModeBase_generated_h
+	#include "Section_01GameModeBase.h"
+#endif
